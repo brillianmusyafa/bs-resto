ba64d4579792ea7143291d5d8304c2b9434c1498
Brillian Musyafa <brillianmusyafa@gmail.com>
2018-09-28 02:54:13 +0700

    initial project bro

---
+++ application/language/indonesia/indonesia_lang.php
@@ -0,0 +1,314 @@
+<?php
+
+// ********************* General *********************
+
+$lang['title'] = "POS - titik penjualan";
+$lang['LnImage'] = "id.png";
+$lang['Close'] = "Tutup";
+$lang['Submit'] = "Kirim";
+$lang['Delete'] = "Hapus";
+$lang['Edit'] = "Sunting";
+$lang['Back'] = "Kembali";
+$lang['Areyousure'] = "Apakah kamu yakin?";
+$lang['Deletemessage'] = "Anda tidak akan dapat memulihkan Data ini nanti!";
+$lang['Deletedmessage'] = "Data telah dihapus.";
+$lang['yesiam'] = "Ya, hapus!";
+$lang['Deleted'] = "Terhapus!";
+$lang['Revenue'] = "Pendapatan";
+
+
+// ********************* Auth *********************
+
+$lang['Username'] = "Nama pengguna";
+$lang['Password'] = "Kata sandi";
+$lang['PasswordRepeat'] = "Ulangi kata kunci";
+$lang['Avatar'] = "Avatar";
+$lang['firstname'] = "nama depan";
+$lang['lastname'] = "Nama keluarga";
+$lang['Role'] = "Peran";
+$lang['RoleAdimn'] = "Admin";
+$lang['RoleSales'] = "Karyawan bagian penjualan";
+$lang['Email'] = "E-mail";
+$lang['lastActive'] = "Aktif terakhir";
+$lang['Login'] = "Masuk";
+$lang['Loginaccount'] = "Selamat Datang";
+$lang['login_incorrect'] = "Login salah";
+$lang['LogOut'] = "Keluar";
+$lang['Adduser'] = "Tambahkan pengguna";
+$lang['users'] = "Pengguna";
+
+// ********************* Menu *********************
+
+$lang['POS'] = "POS";
+$lang['Product'] = "Produk";
+$lang['Setting'] = "Pengaturan";
+$lang['Categories'] = "Kategori";
+$lang['Customers'] = "Konsumen";
+$lang['Sales'] = "Penjual";
+$lang['Reports'] = "Laporan";
+$lang['Expense'] = "Biaya";
+$lang['People'] = "Orang-orang";
+$lang['Suppliers'] = "Pemasok";
+
+// ********************* product page *********************
+// ----infos
+$lang['ProductCode'] = "Kode";
+$lang['ProductName'] = "Nama";
+$lang['Category'] = "Kategori";
+$lang['ProductTax'] = "pajak (%)";
+$lang['Cost'] = "Biaya";
+$lang['Price'] = "Harga";
+$lang['Action'] = "Tindakan";
+$lang['PurchasePrice'] = "Harga pembelian";
+$lang['ProductDescription'] = "Deskripsi Produk";
+$lang['Imageinput'] = "Masukkan Gambar";
+$lang['AddProduct'] = "Tambahkan Produk";
+$lang['ViewImage'] = "Lihat gambar";
+$lang['ProductImage'] = "Gambar";
+$lang['ProductColor'] = "Pilih warna untuk Tampilan di POS.";
+$lang['codeerror'] = "* kode harus unik";
+$lang['ImportProducts'] = "Produk Impor";
+$lang['DownloadCSV'] = "Unduh CSV";
+$lang['UploadCSVfile'] = "Unggah file CSV";
+$lang['DownloadSample'] = "Unduh Contoh";
+$lang['Standard'] = "Standar";
+$lang['Service'] = "Layanan";
+$lang['Type'] = "Mengetik";
+$lang['AlertQt'] = "Kueri Peringatan";
+$lang['ModifyStock'] = "Ubah Stok";
+$lang['Stock'] = "persediaan";
+$lang['Viewproduct'] = "Lihat produk";
+$lang['RowsNumber'] = "nomor kolom";
+$lang['printBarcodes'] = "cetak barcode";
+$lang['combination'] = "kombinasi";
+$lang['combinations'] = "Kombinasi";
+$lang['NoProduct'] = "tidak ditemukan kecocokan";
+$lang['Modify'] = "Memodifikasi";
+$lang['All'] = "Semua";
+$lang['Unit'] = "Unit Produk";
+$lang['inclusive'] = "inklusif";
+$lang['exclusive'] = "eksklusif";
+$lang['TaxMethod'] = "Metode Pajak";
+$lang['ApplyFilter'] = "Terapkan Filter";
+$lang['ProductType'] = "Tipe produk";
+$lang['ProductOptions'] = "Opsi Produk dipisahkan oleh koma (mis: Lebih Banyak Keju, Dimasak, dll ..)";
+
+// ********************* categories page *********************
+
+$lang['CategoryName'] = "Nama Kategori";
+$lang['CreatedAt'] = "Ceat At";
+$lang['AddCategory'] = "Tambahkan kategori";
+
+// ********************* expences page *********************
+
+$lang['Expences'] = "Biaya";
+$lang['ViewFile'] = "Lihat file";
+$lang['Reference'] = "Referensi";
+$lang['Amount'] = "Jumlah";
+$lang['Store'] = "Toko";
+$lang['AddExpence'] = "Tambahkan Biaya";
+$lang['Attachment'] = "Lampiran";
+$lang['AttachmentInfos'] = "File yang diterima (images | pdf | doc | xls | zip) Ukuran maksimum: 2Mb";
+
+// ********************* Stores page *********************
+
+$lang['Stores'] = "Toko";
+$lang['AddStore'] = "Tambahkan Toko";
+$lang['StoreName'] = "Nama Toko";
+$lang['StorePhone'] = "Simpan Telepon";
+$lang['CustomeFooter'] = "Footer Custome untuk penerimaan";
+$lang['manageTables'] = "mengelola Tabel";
+$lang['TableName'] = "Nama / nomor tabel";
+$lang['Addtable'] = "Tambahkan tabel";
+$lang['AddZone'] = "Tambahkan zona";
+$lang['ZoneName'] = "Nama zona";
+$lang['Table'] = "Meja";
+$lang['ChooseZone'] = "Pilih zona";
+$lang['TablesDeleted'] = "Semua tabel yang terkait dengan zona ini akan dihapus";
+
+// ********************* Setting page *********************
+
+$lang['Settings'] = "Pengaturan";
+$lang['Warehouses'] = "Gudang";
+$lang['SettingsDesciption'] = "Perbaharuiaturan";
+$lang['Company'] = "Nama Perusahaan";
+$lang['CompanyLogo'] = "Logo Perusahaan";
+$lang['Phone'] = "Telpon Perusahaan";
+$lang['Currency'] = "Kode Mata Uang";
+$lang['keyboardDisplay'] = "Tampilkan keyboard virtual";
+$lang['ReceiptFooter'] = "Teks pada Footer Resep";
+$lang['ReceiptHeader'] = "Teks pada Header Resep";
+$lang['themesPick'] = "Pilih temamu";
+$lang['DefaultDiscount'] = "Diskon Bawaan";
+$lang['DefualtTax'] = "Pajak Normal";
+$lang['timezone'] = "Mohon pilih zona waktu";
+$lang['Country'] = "Negara";
+$lang['City'] = "Kota";
+$lang['Adresse'] = "Alamat";
+$lang['AddWarehouse'] = "Tambah Gudang";
+$lang['AddWarehouse'] = "Tambah Gudang";
+$lang['WarehouseName'] = "Nama Gudang";
+$lang['WarehousePhone'] = "Telpon Gudang";
+$lang['numberDecimal'] = "jumlah desimal (Pembulatan)";
+
+// ********************* Customer page *********************
+
+$lang['CustomerName'] = "Nama Pelanggan";
+$lang['CustomerPhone'] = "Telepon";
+$lang['CustomerEmail'] = "E-mail";
+$lang['CustomerDiscount'] = "Diskon";
+$lang['AddCustomer'] = "Tambahkan Pelanggan";
+
+// ********************* Waiter page *********************
+
+$lang['WaiterName'] = "Nama Pelayan";
+$lang['withoutWaiter'] = "tanpa Pelayan";
+$lang['WaiterPhone'] = "Telepon";
+$lang['WaiterEmail'] = "E-mail";
+$lang['WaiterStore'] = "Toko";
+$lang['AddWaiter'] = "Tambahkan Pelayan";
+$lang['Waiters'] = "Pelayan";
+$lang['Waiter'] = "Pelayan";
+
+// ********************* Supplier page *********************
+
+$lang['SupplierName'] = "nama pemasok";
+$lang['SupplierPhone'] = "Telepon";
+$lang['SupplierEmail'] = "E-mail";
+$lang['AddSupplier'] = "Tambahkan Pemasok";
+$lang['Supplier'] = "Pemasok";
+
+// ********************* register *********************
+
+$lang['Return'] = "Kembali";
+$lang['RegisterDetails'] = "Detail Pendaftaran";
+$lang['CloseRegister'] = "tutup & nbsp; daftar";
+$lang['SwitchStore'] = "pindah & nbsp; simpan";
+$lang['open'] = "Buka";
+$lang['Stillopen'] = "Masih buka";
+$lang['close'] = "Dekat";
+$lang['closedat'] = "Waktu tutup";
+$lang['ChooseStore'] = "Pilih toko";
+$lang['PayementType'] = "Jenis Pembayaran";
+$lang['EXPECTED'] = "Diharapkan";
+$lang['COUNTED'] = "Terhitung";
+$lang['DIFFERENCES'] = "Perbedaan";
+$lang['PaymentsSummary'] = "Ringkasan Pembayaran";
+$lang['Openingtime'] = "Waktu pembukaan";
+$lang['Openedby'] = "Dibuka oleh";
+$lang['note'] = "Catatan";
+$lang['CashinHand'] = "Uang tunai di tangan";
+$lang['CloseMessageRegister'] = "Anda tidak akan dapat memulihkan Penangguhan nanti!";
+$lang['yesClose'] = "Ya, Tutup!";
+$lang['TotalRevenue'] = "Total pendapatan";
+$lang['ClosedRegister'] = "Register ini ditutup oleh";
+$lang['at'] = "di";
+$lang['Kitchenpage'] = "Buka & nbsp; ke & nbsp; laman Dapur & nbsp;";
+
+
+// ********************* POS page *********************
+
+$lang['AddNewCustomer'] = "Tambahkan Pelanggan Baru";
+$lang['ChooseClient'] = "Pilih Klien";
+$lang['WalkinCustomer'] = "Berjalan di Pelanggan";
+$lang['item'] = "item";
+$lang['TotalItems'] = "Jumlah barang";
+$lang['Discount'] = "Diskon";
+$lang['tax'] = "pajak";
+$lang['GrandTotal'] = "Total keseluruhan";
+$lang['Paid'] = "Berbayar";
+$lang['Change'] = "Perubahan";
+$lang['Tel'] = "Tel:";
+$lang['ShowlastReceipt'] = "Tampilkan Tanda Terima Terakhir";
+$lang['BarcodeScanner'] = "Pemindai kode batang";
+$lang['Product'] = "Produk";
+$lang['price'] = "Harga";
+$lang['Quantity'] = "Kuantitas";
+$lang['SubTotal'] = "SubTotal";
+$lang['OrderTAX'] = "Pesanlah PAJAK";
+$lang['CANCEL'] = "MEMBATALKAN";
+$lang['PAYEMENT'] = "PEMBAYARAN";
+$lang['Search'] = "Pencarian";
+$lang['EmptyList'] = "Daftar Kosong";
+$lang['SelectProduct'] = "Pilih Produk untuk memulai";
+$lang['INVOICE'] = "FAKTUR";
+$lang['invoice'] = "faktur";
+//---- Add Sale
+$lang['empty'] = "Kosong";
+$lang['Receipt'] = "Penerimaan";
+$lang['AddSale'] = "Tambahkan Penjualan";
+$lang['Customer'] = "Pelanggan";
+$lang['Total'] = "Total";
+$lang['paymentMethod'] = "Metode pembayaran:";
+$lang['method'] = "metode:";
+$lang['Cash'] = "Kas";
+$lang['Paid'] = "Berbayar";
+$lang['ReturnChange'] = "Perubahan";
+$lang['CreditCard'] = "Kartu kredit";
+$lang['CreditCardNum'] = "Nomor Kartu Kredit (swipe)";
+$lang['CreditCardHold'] = "Pemegang kartu kredit";
+$lang['CODECV'] = "CV KODE";
+$lang['Year'] = "Tahun";
+$lang['Month'] = "Bulan";
+$lang['Cheque'] = "Memeriksa";
+$lang['ChequeNum'] = "Periksa Nomor";
+$lang['SaleNum'] = "Dijual No";
+$lang['Date'] = "Tanggal";
+$lang['DateReprise'] = "Tanggal pengambilan";
+$lang['print'] = "mencetak";
+$lang['saleStripesccess'] = "Penjualan berhasil ditambahkan";
+$lang['Lowinventory'] = "Inventaris rendah";
+$lang['requiresRegister'] = "membutuhkan pendaftaran terbuka";
+$lang['Options'] = "Pilihan";
+$lang['NoTables'] = "Toko ini tidak memiliki tabel, silakan kunjungi halaman toko untuk menambahkan tabel";
+$lang['CancelAll'] = "Batalkan & nbsp; Semua";
+
+
+// ********************* Sales page *********************
+
+$lang['paid'] = "Berbayar";
+$lang['unpaid'] = "Tidak dibayar";
+$lang['Partiallypaid'] = "Dibayar sebagian";
+$lang['AddPayement'] = "Tambahkan Pembayaran";
+$lang['changeClient'] = "Ubah Klien";
+$lang['changeStatus'] = "Merubah status";
+$lang['Createdby'] = "Ceat By";
+$lang['Number'] = "Jumlah";
+$lang['Status'] = "Status";
+$lang['Payements'] = "Pembayaran";
+$lang['AddPayements'] = "Tambahkan Pembayaran";
+
+// ********************* Report page *********************
+
+$lang['Customers'] = "Pelanggan";
+$lang['in'] = "di";
+$lang['TodaySale'] = "Penjualan Hari Ini";
+$lang['monthlyStats'] = "Statistik bulanan";
+$lang['TopProducts'] = "5 Produk Teratas tahun ini";
+$lang['ClientsStats'] = "Statistik Klien";
+$lang['SelectClient'] = "Pilih Klien";
+$lang['ProductsStats'] = "Produk Stats";
+$lang['SelectProduct'] = "Pilih Produk";
+$lang['SelectRange'] = "Pilih Rentang";
+$lang['Product'] = "Produk";
+$lang['GetReport'] = "Dapatkan Laporan";
+$lang['Profit'] = "Keuntungan";
+$lang['TotalProfit'] = "Total keuntungan";
+$lang['RegisterStats'] = "Daftarkan Statistik";
+$lang['SelectStore'] = "Pilih Simpan";
+
+//---- Add Sale
+$lang['January'] = "Januari";
+$lang['February'] = "Februari";
+$lang['March'] = "Maret";
+$lang['April'] = "April";
+$lang['May'] = "Mungkin";
+$lang['June'] = "Juni";
+$lang['July'] = "Juli";
+$lang['August'] = "Agustus";
+$lang['September'] = "September";
+$lang['October'] = "Oktober";
+$lang['November'] = "November";
+$lang['December'] = "Desember";
+
+?>
